7e026015000af21180b69df9588f4be6bb7580b6
Alex Vukadinov <alexvuka1@gmail.com>
2023-07-19 10:20:06 +0100

    fixed env variables in migration script and type module in package.json

--- src/server/db/migrate.ts
+++ src/server/db/migrate.ts
@@ -2,10 +2,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 import { exit } from 'process';
+import { env } from '../../env.mjs';
 
-const sql = postgres(process.env.DATABASE_URL ?? '', {
+const sql = postgres(env.DATABASE_URL ?? '', {
   max: 1,
-  ssl: process.env.VERCEL_ENV !== 'development' ? 'require' : void 0,
+  ssl: env.NODE_ENV !== 'development' ? 'require' : void 0,
 });
 const db = drizzle(sql);
 
